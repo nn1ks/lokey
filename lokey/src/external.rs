63523e51d3998d7ab33ac24b4f19001fae8f34b3
Niklas Sauter <niklas@n1ks.net>
2024-06-02 15:18:25 +0200

    Move `external::Channel` to separate file

--- lokey/src/external.rs
+++ lokey/src/external.rs
@@ -1,20 +1,21 @@
 #[cfg(feature = "ble")]
 pub mod ble;
+mod channel;
 pub mod empty;
 #[cfg(feature = "usb")]
 pub mod usb;
 #[cfg(all(feature = "usb", feature = "ble"))]
 pub mod usb_ble;
 
+pub use channel::{Channel, DynChannel};
+
 use crate::internal;
-use crate::util::pubsub::{PubSubChannel, Subscriber};
 use crate::{mcu::Mcu, Device};
 use alloc::boxed::Box;
 use core::any::Any;
 use core::future::Future;
 use core::pin::Pin;
 use embassy_executor::Spawner;
-use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 
 pub type DeviceChannel<D> =
     <<D as Device>::ExternalChannelConfig as ChannelConfig<<D as Device>::Mcu>>::Channel;
@@ -130,61 +131,5 @@ pub trait ChannelImpl: Any {
     fn send(&self, message: Message);
     fn wait_for_activation_request(&self) -> Pin<Box<dyn Future<Output = ()> + '_>> {
         Box::pin(core::future::pending())
-    }
-}
-
-static INNER_CHANNEL: PubSubChannel<CriticalSectionRawMutex, Message> = PubSubChannel::new();
-
-pub type DynChannel = Channel<dyn ChannelImpl>;
-
-pub struct Channel<T: ?Sized + 'static> {
-    inner: &'static T,
-}
-
-impl<T: ChannelImpl> Channel<T> {
-    /// Creates a new external channel.
-    ///
-    /// This method should not be called, as the channel is already created by the [`device`](crate::device) macro.
-    pub fn new(inner: &'static T) -> Self {
-        Self { inner }
-    }
-
-    /// Converts this channel into a dynamic one.
-    ///
-    /// This can be useful if you want to pass the channel to an embassy task as they can't have
-    /// generic parameters.
-    pub fn as_dyn(&self) -> DynChannel {
-        Channel { inner: self.inner }
-    }
-}
-
-impl<T: ChannelImpl + ?Sized> Channel<T> {
-    pub fn send(&self, message: Message) {
-        INNER_CHANNEL.publish(message.clone());
-        self.inner.send(message);
-    }
-
-    pub fn receiver(&self) -> Receiver {
-        Receiver {
-            subscriber: INNER_CHANNEL.subscriber(),
-        }
-    }
-}
-
-impl<T: ?Sized> Clone for Channel<T> {
-    fn clone(&self) -> Self {
-        *self
-    }
-}
-
-impl<T: ?Sized> Copy for Channel<T> {}
-
-pub struct Receiver {
-    subscriber: Subscriber<'static, CriticalSectionRawMutex, Message>,
-}
-
-impl Receiver {
-    pub async fn next(&mut self) -> Message {
-        self.subscriber.next_message().await
     }
 }
