1f8639ba37c171b5445931524c055e1897990d0e
Niklas Sauter <niklas@n1ks.net>
2025-04-08 23:03:27 +0200

    Update imports

--- lokey/src/mcu/nrf52840/ble/external.rs
+++ lokey/src/mcu/nrf52840/ble/external.rs
@@ -3,11 +3,10 @@ mod server;
 
 use super::{BLE_ADDRESS_WAS_SET, device_address_to_ble_address};
 use crate::external::ble::{Event, Message};
-use crate::external::{self};
 use crate::mcu::{Nrf52840, Storage};
 use crate::util::channel::Channel;
 use crate::util::{debug, error, info, unwrap, warn};
-use crate::{Address, internal};
+use crate::{Address, external, internal};
 use alloc::boxed::Box;
 use alloc::vec::Vec;
 use bonder::Bonder;
